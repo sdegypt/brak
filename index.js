f84d37b7947a94d18326263be8ca3b12d12f1a02
sdegypt <egyptsd249@gmail.com>
2025-06-23 11:19:41 +0300

    Update index.js

--- index.js
+++ index.js
@@ -181,7 +181,9 @@ cron.schedule('0 0 * * *', async () => {
 app.use(errorHandler);
 
 // بدء الخادم
-const PORT =3000;
-server.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
+
